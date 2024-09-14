d64ed93d99585f86bb701052f9f7c8a522c07f8b
KGMA74 <marmelkambou2@gmail.com>
2024-09-14 00:29:44 +0000

    change in api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,6 +4,38 @@ const API_URL = 'http://127.0.0.1:8000/api';
 //const API_URL = "http://192.168.31.253:8000/api";
 
 
+
 export const api = ky.extend({
-    prefixUrl: API_URL, //process.env.API_URL;                                                                                                                                                                                  
-}); 
+    prefixUrl: API_URL, //process.env.API_URL; 
+    credentials: 'include', 
+    hooks: {
+        afterResponse: [
+            async (request, options, response) => {
+                if (response.status === 401) {
+                    try {
+                        await ky.post(`${API_URL}/jwt/refresh/`, {credentials: 'include'})
+
+                        const relativeUrl = request.url.replace(API_URL+'/', '');
+
+                        // Réessaie la requête originale avec le nouveau token
+                        const retryResponse = await api(relativeUrl, {
+                            ...options,
+                            headers: {
+                                ...options.headers,
+                            },
+                        });
+
+                        return retryResponse;
+                    } catch (error) {
+                        console.error('Error refreshing token', error);
+                        throw error; 
+                    }
+                }
+
+                return response;  // Retourne la réponse originale si pas de 401
+            }
+        ]
+    }
+});
+
+export default api;
